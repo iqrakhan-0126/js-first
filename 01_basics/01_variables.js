28fa29f1e795ad253aeb196b6e88dab5989f67d0
iqra khan <iqrakhan2004.0@gmail.com>
2025-10-13 18:47:26 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12234
+let accountEmail = "iqra94i3@gmail.com"
+var accountPassword = "12345"
+accountCity = "pune"
+
+// accountId = 2
+accountEmail = "iqe@gmail.com"
+accountPassword = "123"
+accountCity= "sangli"
+
+
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" ; //treat all JS code as newer version
+
+//alert( 3+4) we are using nodejs , not browser
+
+console.log(3
+    +3) //code readability should be high
+
+console.log("iqra");
+
+let name = "iqra"
+let age = 21
+let car = true
+
+console.log(typeof null)
+    
+// data types in javascript
+
+// 1) Number = 2 to power 34
+// 2) BigInt
+// 3) String => ""
+// 4) boolean =>true/false
+// 5) null => standalone value
+// 6) undefined =>
+// 7) Symbol =>unique
