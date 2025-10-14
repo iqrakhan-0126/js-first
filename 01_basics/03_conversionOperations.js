bd335a97db3a51b63cb371d6ddcd6f292f807574
iqra khan <iqrakhan2004.0@gmail.com>
2025-10-14 17:42:59 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = null
+console.log(typeof score)
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" =>33
+// "33aa" => NaN
+// true => 1 ; false => 0
+
+
+let isLoggedIn ="iqra"
+let value = Boolean(isLoggedIn)
+console.log(value)
+
+//1 => true ;
+//0 => false;
+//"" => false;
+// "iqra" => true;
+
+let someNum = 33
+
+let num =  String(someNum)
+console.log(num)
+console.log(typeof num)
