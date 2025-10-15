a3ef4cfb249bf7a27c53bc7dc446c8fd7d6b5f49
iqra khan <iqrakhan2004.0@gmail.com>
2025-10-15 17:43:17 +0000

    commit the changes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -25,3 +25,29 @@ let someNum = 33
 let num =  String(someNum)
 console.log(num)
 console.log(typeof num)
+
+//********************* Operations ***************************
+
+let nume = 33
+console.log(num + 2)
+console.log(9 +2);
+console.log("1" + 2);
+console.log(2 + 3 +"1");
+console.log((2+3) + 2 + (3+"3"));
+
+let str2 = "iqra"
+let str3 = " khan"
+let str1 = str2 + str3
+console.log(str1);
+
+
+// console.log(+true);
+
+let y = 2
+let z = y--
+console.log(y,z)
+
+
+let gameCount = 20
+gameCount**
+console.log(gameCount)
